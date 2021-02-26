6b61fc475c70635d917882bb4d74e4bc2b97cf23
Boes gregory <gregoryboes33@gmail.com>
2021-02-26 11:28:22 +0100

    end but no responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,30 @@
 
 const getApi = () => {
 
-fetch("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=VefOabv7aaAcANxaq5BF06ebhqaWERS8")
+fetch("https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=UsgAbfRs27wIEecUu2mX5KKyhF67z8NA")
 	
 .then(response => response.json()) 
 .then(data => {
 
   const articles = data.results;
-  for(let i = 0; i < articles.length; i++) {
+  for(let i = 0; i < 100; i++) {
            
     var squelette = document.createElement("div");
     squelette.classList.add("col-3", "d-flex", "justify-content-center", "align-self-start" )
-   
-    squelette.innerHTML =
+    const date = articles[i].updated_date;
+    
+    const newDate = date.replace(/T\d+[:]\d+[:]\d+[-]/, ' ');
+
+    if (articles[i].multimedia != null) {
+
+      squelette.innerHTML =
       `
        <div class="card">
         <div class="imgBx">
-          <img src=${articles[i].media[0]["media-metadata"][2].url} class="card-img-top" alt="article image">
+          <img src=${articles[i].multimedia[2].url} class="card-img-top" alt="article image">
         </div>
         <div class="card-body">
+          <p class="card-text fw-light fst-italic date"><i>Date: ${newDate}</i></p>
           <p class="card-text fw-light fst-italic" >Type: <i>${articles[i].section}</i></p>
           <p class="card-title text-secondary"><b>${articles[i].title}</b></p>
           <p class="card-text">${articles[i].abstract}</p>
@@ -27,6 +33,9 @@ fetch("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=VefOabv
         </div>
       </div>  `
     returnApi.append(squelette);
+
+    }
+  
 
 
    } 
